bc1a56d84d231eabdca89d5f684d32b5d69c7f78
parrt <parrt@cs.usfca.edu>
2017-10-25 17:04:18 -0700

    play with rf

--- housing/rf.py
+++ housing/rf.py
@@ -5,14 +5,17 @@ http://www.dcc.fc.up.pt/~ltorgo/Regression/cal_housing.tgz
 """
 from sklearn.model_selection import train_test_split
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 import math
+import numpy as np
 
 housing = pd.read_csv('../data/CaliforniaHousing.csv')
 #":type : pd.core.frame.DataFrame"
 
-print(housing.columns)
+#print(housing.describe())
 X, y = housing.drop('medianHouseValue', axis=1), housing['medianHouseValue']
+
+# y = np.log(y)
 
 # split 80/20 train-test
 # N = len(X)
@@ -25,11 +28,30 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 # print(X_train)
 # print(y_train)
 
-regr = RandomForestRegressor(n_estimators=100, max_features="sqrt", max_leaf_nodes=3, oob_score=True)
-regr.fit(X_train, y_train)
+def boost():
+    gbrt=GradientBoostingRegressor(n_estimators=100)
+    gbrt.fit(X_train, y_train)
+    y_pred=gbrt.predict(X_test)
+    print("Feature Importances")
+    print(gbrt.feature_importances_)
 
-print(regr.feature_importances_)
-print(regr.oob_score_)
+    #Let's print(the R-squared value for train/test. This explains how much of the variance in the data our model is
+    #able to decipher.
+    print("R-squared for Train: %.2f" %gbrt.score(X_train, y_train))
+    print("R-squared for Test: %.2f" %gbrt.score(X_test, y_test))
 
-prediction = regr.predict(X_test)
-print(prediction - y_test)
+    print((y_pred - y_test)/y_test)
+
+def rf():
+    regr = RandomForestRegressor(n_estimators=100, max_features="sqrt", max_leaf_nodes=15, oob_score=True)
+    regr.fit(X_train, y_train)
+
+    print(regr.feature_importances_)
+    print(regr.oob_score_)
+
+    y_pred = regr.predict(X_test)
+    # print(y_pred - y_test)
+    print("R-squared for Train: %.2f" % regr.score(X_train, y_train))
+    print("R-squared for Test: %.2f" % regr.score(X_test, y_test))
+
+rf()
